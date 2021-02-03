d38051417f270f7c061c8bc6da9a8ce7fad06bf0
micha-lmxt <micha-lmxt@gradientdescent.de>
2021-02-03 22:36:28 +0100

    a

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	export let name: string;
+import {converter} from 'culori';
+
+	const conv = converter("rgb");
+	const canv;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <canvas bind:this={canv} width="400" height="400"  />
+	
 </main>
 
 <style>
@@ -13,18 +16,10 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+    canvas{
+      background-color:rgb(233,233,222);
+      }
+</style
