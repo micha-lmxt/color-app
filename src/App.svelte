301e39b1c56d39800f9dce134f4b5d8eee1c6067
micha-lmxt <micha-lmxt@gradientdescent.de>
2021-02-03 23:37:49 +0100

    r

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
 import {converter} from 'culori';
+import {onMount} from 'svelte';
 
 	const conv = converter("rgb");
 	let canv;
+	onMount(()=>{
+	    const ctx = canv.getContext('2d');
+	    var pixel = ctx.getImageData(0, 0, 400, 400);
+	    const data = pixel.data;
+	    for (let i=0; i < 400; i++){
+	        for (let j=0; j<400;j++){
+	           const a = i - 200;
+	           const b = j - 200;
+	           const r = Math.sqrt(a * a + b * b);
+	           const ind = 4 * (400 * i + j);
+	           if (r > 200){
+	               data[ind]=data[ ind+1]=data[ind+2]=0;
+	               data[ ind+3]=1;
+	               continue;
+	           }
+	           
+	        }
+	    }
+	    ctx.putImageData(pixel, 0, 0);
+	})
 </script>
 
 <main>
